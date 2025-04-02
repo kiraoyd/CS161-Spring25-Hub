c7ef137b029e79cbbd0fa46d4f6b549aa199fe46
Kira <kirak@pdx.edu>
2025-04-02 16:56:28 -0700

    Adding all the content for Week One

---
+++ Practice Problems/Week One/Planetary-Weight-Converter/planets.py
@@ -0,0 +1,12 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Planetary Weights Calculator
+############################################################################
+
+# When you are ready to submit the complete code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "planets.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
---
+++ Practice Problems/Week One/Temperature-converter/temperature.py
@@ -0,0 +1,25 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Temperature Converter
+############################################################################
+
+# When you are ready to submit the complete code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "temperature.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
+
+
+
+
+
+
+
+
+
+
+
+
+
