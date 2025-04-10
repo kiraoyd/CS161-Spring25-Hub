09ba98acf7aac10abf8e831b130d756ae358b089
Kira Klingenberg <kirak@pdx.edu>
2025-04-10 08:24:50 -0700

    Testing something

---
+++ Practice-problems/Week Two/f_to_c.py
@@ -0,0 +1,12 @@
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Temperature Converter: F to C
+# To run: open the terminal, type python3 f_to_c.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "f_to_c.ipynb" below 👇👇
+# Download this f_to_c.py file, and upload it to the associated Canvas assignment
