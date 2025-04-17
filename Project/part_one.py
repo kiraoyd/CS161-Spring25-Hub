ec8aa34995f5b70b008665a8c0f5150488f62c59
Kira Klingenberg <kirak@pdx.edu>
2025-04-17 13:27:30 -0700

    Added folder and file for the project

---
+++ Project/part_one.py
@@ -0,0 +1,15 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: 
+# Rename your .py file to: part1_<studentname>.py, replacing <studentname> with your name
+
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# Test and run your code to make sure it works!
+# Save your work.
+# Download this codefile, and upload it to the Project Part One Canvas Assignment along with your documentation file
