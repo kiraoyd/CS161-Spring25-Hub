f18e72095fe216855e2596445ee9cfea243676a3
Kira Klingenberg <kirak@pdx.edu>
2025-04-21 16:43:38 -0700

    Added practice problem for Week Four Tuesday

---
+++ Graded-Practice-Problems/4-Week-Four/Guess/guess.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Guess the Number
+# To run: open the terminal, type python3 samesies.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "guess.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
---
+++ Graded-Practice-Problems/4-Week-Four/Pizza-Comparison/pizza_comparison.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Pizza Comparison Program
+# To run: open the terminal, type python3 pizza_comparison.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "pizza_comparison.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
