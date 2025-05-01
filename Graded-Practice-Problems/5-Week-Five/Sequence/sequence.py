480a8c2aeeff6aafe0b8592dd43f5fde9a0ac661
Kira Klingenberg <kirak@pdx.edu>
2025-05-01 13:28:24 -0700

    Adding loop content for 5/1

---
+++ Graded-Practice-Problems/5-Week-Five/Sequence/sequence.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Show Sequence
+# To run: open the terminal, type python3 sequence.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "sequence.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
