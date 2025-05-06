2364470a9d2899a335343ae59f71fd971079a7e6
Kira Klingenberg <kirak@pdx.edu>
2025-05-06 12:29:04 -0700

    Bringing repo up to date for 5/6

---
+++ Class-notes/globals.py
@@ -0,0 +1,35 @@
+#Global Space
+import math
+
+#
+MY_PI = math.PI
+DAMAGE_MAX = 20
+
+drink_counter = 0
+
+def eat():
+    global drink_counter
+    hunger = 10
+    while hunger > 0:
+        print("Eat more!")
+        hunger = hunger - 3
+
+    drink_counter = 200
+
+
+def drink():
+    global drink_counter
+    thirst = 5
+    while thirst > 0:
+        print("Sip water...")
+        thirst = thirst - 1
+        drink_counter = drink_counter + 1
+
+        
+def main():
+    global drink_counter
+    drink()
+    eat()
+    print(drink_counter)
+
+main()
