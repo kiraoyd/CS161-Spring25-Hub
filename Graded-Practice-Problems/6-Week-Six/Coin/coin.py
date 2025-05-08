5fed3526844a9560ce7e261728b40451a061012b
Kira Klingenberg <kirak@pdx.edu>
2025-05-08 08:56:00 -0700

    Adding content for week six day two, monte carlo problems

---
+++ Graded-Practice-Problems/6-Week-Six/Coin/coin.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Coin Flip Monte Carlo Probability
+# To run: open the terminal, type python3 coin.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "coin.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
