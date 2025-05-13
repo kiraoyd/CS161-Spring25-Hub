c4f4ab464644bad517a14d72af156909c9130e57
Kira Klingenberg <kirak@pdx.edu>
2025-05-13 12:44:07 -0700

    Added week 7 Tuesday content

---
+++ Graded-Practice-Problems/7-Week-Seven/Average/average.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Average of User Data
+# To run: open the terminal, type python3 average.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "average.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
---
+++ Graded-Practice-Problems/7-Week-Seven/Monte-Carlo-Menu/monte_menu.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Monte Carlo Menu
+# To run: open the terminal, type python3 monte_menu.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "monte_menu.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
