75a65dbf93266712041dce339966d28d19e256da
Kira Klingenberg <kirak@pdx.edu>
2025-05-26 22:33:58 -0700

    Adding content for Week 9, moved Largest folder from week 8 to week 9

---
+++ Graded-Practice-Problems/9-Week-Nine/EXTRA-CREDIT/steps.py
@@ -0,0 +1,13 @@
+
+############################################################################
+# --------------------- HEADER COMMENTS------------------------------------#
+# NAME:
+# DATE:
+# CLASS:
+# Program Title: Step Counter
+# To run: open the terminal, type python3 steps.py
+############################################################################
+
+# When you are ready to submit the completed code to Canvas...
+# 👇👇 Copy and paste in your code from the notebook "steps.ipynb" below 👇👇
+# Download this .py file, and upload it to the associated Canvas assignment
